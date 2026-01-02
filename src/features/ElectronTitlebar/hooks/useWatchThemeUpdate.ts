c8211356c9b424bb25884bc394af00a0595b36f3
Innei <tukon479@gmail.com>
2026-01-03 00:35:25 +0800

    refactor: migrate theme management to `next-themes` and remove theme from route variants and global store.
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/features/ElectronTitlebar/hooks/useWatchThemeUpdate.ts
+++ src/features/ElectronTitlebar/hooks/useWatchThemeUpdate.ts
@@ -4,8 +4,6 @@ import { useLayoutEffect } from 'react';
 
 import { useElectronStore } from '@/store/electron';
 import { useGlobalStore } from '@/store/global';
-import { systemStatusSelectors } from '@/store/global/selectors';
-import { ensureElectronIpc } from '@/utils/electron/ipc';
 
 export const useWatchThemeUpdate = () => {
   const [isAppStateInit, systemAppearance, updateElectronAppState, isMac] = useElectronStore(
@@ -16,14 +14,7 @@ export const useWatchThemeUpdate = () => {
       s.appState.isMac,
     ],
   );
-  const [switchThemeMode, switchLocale] = useGlobalStore((s) => [
-    s.switchThemeMode,
-    s.switchLocale,
-  ]);
-
-  useWatchBroadcast('themeChanged', ({ themeMode }) => {
-    switchThemeMode(themeMode, { skipBroadcast: true });
-  });
+  const [switchLocale] = useGlobalStore((s) => [s.switchLocale]);
 
   useWatchBroadcast('localeChanged', ({ locale }) => {
     switchLocale(locale as any, { skipBroadcast: true });
@@ -32,10 +23,6 @@ export const useWatchThemeUpdate = () => {
   useWatchBroadcast('systemThemeChanged', ({ themeMode }) => {
     updateElectronAppState({ systemAppearance: themeMode });
   });
-  const themeMode = useGlobalStore(systemStatusSelectors.themeMode);
-  useLayoutEffect(() => {
-    ensureElectronIpc().system.setSystemThemeMode(themeMode);
-  }, []);
 
   useLayoutEffect(() => {
     if (!isAppStateInit || !isMac) return;
